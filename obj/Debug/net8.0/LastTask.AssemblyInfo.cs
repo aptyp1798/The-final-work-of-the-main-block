f8b495d90542dd24c6f26bd7e113190462bbfa87
aptyp1798 <aptyp179811111@gmail.com>
2024-07-19 05:39:31 +0700

    Add text in ReadME file

--- obj/Debug/net8.0/LastTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/LastTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LastTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f855e49204ed25a015fec2b9a43cdcba0d86395a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58284328f692c65e6167a89fab881d0efd9e706b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LastTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LastTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
